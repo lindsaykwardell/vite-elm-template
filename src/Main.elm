ca766fd3bb543726dedbcdf49461a2e498754e1a
Lindasy Wardell <three060@gmail.com>
2021-08-29 19:34:49 -0700

    fix image imports

--- src/Main.elm
+++ src/Main.elm
@@ -26,6 +26,6 @@ update msg model =
 view : Int -> Html Msg
 view model =
     div []
-        [ img [ src "./src/assets/logo.png", style "width" "300px" ] []
+        [ img [ src "/logo.png", style "width" "300px" ] []
         , helloWorld model
         ]
