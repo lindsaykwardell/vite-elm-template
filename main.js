9c6f9263b4c9e5e2cf46be2dcaeebcd19e843015
Lindsay Wardell <three060@gmail.com>
2022-07-13 20:48:26 +0000

    Revert from Vite 3, add Elm Debug Transformer

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 import "./style.css";
 import { Elm } from "./src/Main.elm";
 
+if (process.env.NODE_ENV === "development") {
+    const ElmDebugTransform = await import("elm-debug-transformer")
+
+    ElmDebugTransform.register({
+        simple_mode: true
+    })
+}
+
 const root = document.querySelector("#app div");
 const app = Elm.Main.init({ node: root });
